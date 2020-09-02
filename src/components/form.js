5446e5309cf954d70019c51769e9d4371ca30085
Matt Loniero <matt@stillwateragency.com>
2020-09-02 14:53:01 -0700

    updating form action

--- src/components/form.js
+++ src/components/form.js
@@ -61,7 +61,7 @@ class Form extends React.Component {
     console.log(formData)
 
     axios
-      .post("https://alp-nu.vercel.app/api/form-save", formData)
+      .post("/api/form-save", formData)
       .then(res => {
         console.log(res.data)
         window.location = "/thankyou"
